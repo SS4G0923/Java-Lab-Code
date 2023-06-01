3ebb605c9a8bfe8ef8a209784cfdee2f92d63eaf
Zheng Lian <42827496+SS4G0923@users.noreply.github.com>
2023-06-01 08:17:08 +0800

    Add files via upload

---
+++ Lab11/OperatorListener.java
@@ -0,0 +1,4 @@
+package Lab11;
+
+public class OperatorListener {
+}
---
+++ Lab11/WindowsView.java
@@ -0,0 +1,7 @@
+package Lab11;
+
+import javax.swing.*;
+
+public class WindowsView extends JFrame {
+    JTextField firstNumber = new JTextField(10);
+}
